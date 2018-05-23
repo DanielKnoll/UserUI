f4ce3b69872033ee6f69b83b3343fdae3a1fc80a
Dani Knoll <knoll.dani@gmail.com>
2018-05-23 12:45:44 +0200

    Click event and conditions on log in and reg button.

--- src/app/navbar/logreg/logreg.component.ts
+++ src/app/navbar/logreg/logreg.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-logreg',
-  templateUrl: './logreg.component.html',
-  styleUrls: ['./logreg.component.css']
+    selector: 'app-logreg',
+    templateUrl: './logreg.component.html',
+    styleUrls: ['./logreg.component.css']
 })
 export class LogregComponent implements OnInit {
+    isLoggedIn:  boolean = false;
+    userName: string = 'Elek Teszt';
+    logOutServerPath: string = 'http://0.0.0.0:8080/api/logout';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onLogOut() {
+        this.isLoggedIn = false;
+    }
+
+    onLogIn() {
+        this.isLoggedIn = true;
+    }
+
+    onRegister() {
+        this.isLoggedIn = true;
+    }
 }
