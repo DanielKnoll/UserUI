96aed978b8f8991ea8787ae07789754ffb60d341
Dani Knoll <knoll.dani@gmail.com>
2018-05-23 15:00:48 +0200

    ng bootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,17 +9,18 @@ import { UserListComponent } from './user-list/user-list.component';
 import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    LogregComponent,
-    UserListComponent,
-    ModalComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        LogregComponent,
+        UserListComponent,
+        ModalComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
