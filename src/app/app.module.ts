498e941b5302dfabdbdd8c7493e547863976a9cb
Dani Knoll <knoll.dani@gmail.com>
2018-05-23 10:49:04 +0200

    Components created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogregComponent } from './navbar/logreg/logreg.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogregComponent,
+    UserListComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
